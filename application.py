2ae8b2d6bff17c9d6455c1c40af372c28869fc4a
CarelBrian <127103318+CarelBrian@users.noreply.github.com>
2024-07-03 19:24:23 +0100

    Deuxième autre version de chebyshev_distance

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 def chebyshev_distance(p1, p2):
-    """Première autre version de chebyshev_distance"""
-    return max(map(lambda xy: abs(xy[0] - xy[1]), zip(p1, p2)))
+    """Deuxième autre version de chebyshev_distance"""
+    max_distance = 0
+    for x, y in zip(p1, p2):
+        max_distance = max(max_distance, abs(x-y))
+        return max_distance
 
 if __name__ == "__main__":
     point1 = (1, 2, 3)
