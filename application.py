8640065fa25de283195345ad1859056dcc9f9843
Samath_ <abdousamathyade@gmail.com>
2024-07-03 18:29:34 +0000

    Ajout de la deuxieme fonction de chebyshev_distance

--- application.py
+++ application.py
@@ -7,3 +7,11 @@ if __name__ == "__main__":
     point2 = (4, 5, 6)
     distance = chebyshev_distance(point1, point2)
     print("Chebyshev Distance:", distance)
+
+def chebyshev_distance2(point1, point2):
+    max = 0
+    for a, b in zip(point1, point2):
+        distance = abs(a - b)
+        if distance > max:
+            max = distance
+    return max
