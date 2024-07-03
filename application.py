6daa5f02cf71049a0aafe4b3567b4367bae0d03a
12alain <kiemdealain02@gmail.com>
2024-07-03 18:34:15 +0000

    troisieme facon avec les tensor

--- application.py
+++ application.py
@@ -1,20 +1,19 @@
-import numpy as np
+import torch
 
 def chebyshev_distance(p1, p2):
-    """Calcule la distance de Chebyshev entre deux points p1 et p2."""
-    # Convertissons en tabelau numpy
-    p1 = np.array(p1)
-    p2 = np.array(p2)
+    """Calcule la distance de Chebyshev entre deux points p1 et p2 en utilisant PyTorch."""
+    # Convertissons  les points en tensors PyTorch
+    p1 = torch.tensor(p1, dtype=torch.float32)
+    p2 = torch.tensor(p2, dtype=torch.float32)
     # Calculer les différences absolues
-    differences = np.abs(p1 - p2)
-    #  calcul de la distance maximal 
-    return np.max(differences)
+    differences = torch.abs(p1 - p2)
+    # Retourner la distance maximale trouvée (distance de Chebyshev)
+    return torch.max(differences).item()
 
 if __name__ == "__main__":
-    
+
     point1 = (1, 2, 3)
     point2 = (4, 5, 6)
-    
     distance = chebyshev_distance(point1, point2)
     # Affichage de la distance de Chebyshev
     print("Distance de Chebyshev:", distance)
