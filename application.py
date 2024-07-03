d2d6b94155118f1d5d35e337867f4e687741e313
12alain <kiemdealain02@gmail.com>
2024-07-03 18:27:30 +0000

    deuxieme facon

--- application.py
+++ application.py
@@ -1,20 +1,20 @@
+import numpy as np
+
 def chebyshev_distance(p1, p2):
     """Calcule la distance de Chebyshev entre deux points p1 et p2."""
-    # Initialisation de la distance maximale
-    max_distance = 0
-   
-    for x, y in zip(p1, p2):
-        # Calcul de valeur absolue entre x et  y 
-        distance = abs(x - y)
-        # calcul de la distance maximal 
-        if distance > max_distance:
-            max_distance = distance
-   
-    return max_distance
+    # Convertissons en tabelau numpy
+    p1 = np.array(p1)
+    p2 = np.array(p2)
+    # Calculer les différences absolues
+    differences = np.abs(p1 - p2)
+    #  calcul de la distance maximal 
+    return np.max(differences)
 
 if __name__ == "__main__":
     
     point1 = (1, 2, 3)
     point2 = (4, 5, 6)
+    
     distance = chebyshev_distance(point1, point2)
+    # Affichage de la distance de Chebyshev
     print("Distance de Chebyshev:", distance)
