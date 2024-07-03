9958f67b3ae0d2096f21331da826c122ee3079fa
ChercheurNoir <ali.sonybera@yahoo.com>
2024-07-03 18:20:06 +0000

    Calcul de la distance de Chebyshev entre deux points p1 et p2

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
 def chebyshev_distance(p1, p2):
-    """Docstring..."""
-    return max(abs(x - y) for x, y in zip(p1, p2))
+    """Calcul de la distance de Chebyshev entre deux points p1 et p2."""
+    max_distance = 0
+    for x, y in zip(p1, p2):
+        distance = abs(x - y)
+        if distance > max_distance:
+            max_distance = distance
+    return max_distance
 
 if __name__ == "__main__":
     point1 = (1, 2, 3)
