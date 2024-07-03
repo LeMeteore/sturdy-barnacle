9bb5aec7cea2f97f9573bffceb03b1276ba37304
bmwathie <61660930+bmwathie@users.noreply.github.com>
2024-07-03 18:34:21 +0000

    Maniere 1 : Implementation iterative de la fonction chebyshev_distance

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 def chebyshev_distance(p1, p2):
     """Docstring..."""
-    return max(abs(x - y) for x, y in zip(p1, p2))
+    assert len(p1) == len(p2)
+    distance = 0
+    for i in range(len(point1)):
+        distance = max(distance, abs(point1[i] - point2[i]))
+    return distance
 
 if __name__ == "__main__":
     point1 = (1, 2, 3)
